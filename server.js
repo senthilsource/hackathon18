f58fbec138bae81043d7935875b1a7f2b0517fbd
SenthilKumar Ramasamy <senthilsource@gmail.com>
2018-09-12 01:18:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ var STREAM_SECRET = process.argv[2],
 
 const Secureserver = new https.createServer();
 
-var socketServer = new WebSocket.Server({server:Secureserver, port:8080});
+var socketServer = new WebSocket.Server({port: (process.env.PORT || 5000)}); 
 console.log(socketServer);
 socketServer.connectionCount = 0;
 socketServer.on('connection', function(socket, upgradeReq) {
